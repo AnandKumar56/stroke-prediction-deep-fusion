9a2d4e109a6e85b2f43ca7ed82b2a8cd503f2439
Anand Kumar <akn915573@gmail.com>
2025-07-02 23:24:51 +0530

    fix: update model loading and LSTM input shape handling in models.py

---
+++ FRONTEND/webapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+#from keras.models import load_model
+#import cv2
+import numpy as np
+import pickle
+#from tensorflow.keras.preprocessing import image
+#import tensorflow as tf
+import json
+from PIL import Image
+import joblib
+import numpy as np
+
+
+cnn = pickle.load(open('cnn.pkl', 'rb'))
+lstm = pickle.load(open('lstm.pkl', 'rb'))
+cnn_lstm = pickle.load(open('cnn_lstm.pkl', 'rb'))
+
+def predict(lst, algo):
+    test = np.array(lst)
+    if algo == 'lstm':
+        test = np.reshape(test, (1, 1, -1))  # (batch, timesteps, features)
+        y_pred = lstm.predict(test)
+        return y_pred
+    else:
+        test = np.reshape(test, (1, -1))  # (batch, features)
+        if algo == 'cnn':
+            y_pred = cnn.predict(test)
+            return y_pred
+        else:
+            y_pred = cnn_lstm.predict(test)
+            return y_pred
